d6e29518652f9ec4e345ee469439038da421516c
jhons1206 <webdeveloper84@gmail.com>
2023-01-23 00:09:31 -0500

    Sección 06: Estructuras Condicionales - COMPLETADO

---
+++ 06-Estructuras-condicionales/01-if.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+// Estructura Secuencial
+echo "Primer parrafo <br>";
+// echo "Segundo parrafo <br>";
+echo "Tercer parrafo <br>";
+
+
+// Estructura Condicional If
+if( $a > $b ) {
+    echo "Segundo parrafo <br>";
+}
---
+++ 06-Estructuras-condicionales/02-if-else.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+// Estructura Secuencial
+// echo "Primer parrafo <br>";
+// echo "Segundo parrafo <br>";
+// echo "Tercer parrafo <br>";
+
+
+// Estructura Condicional If else
+// if( $a < $b ) {
+//     echo "Segundo parrafo <br>";
+// } else {
+//     echo "Segundo parrafo editado <br>";
+// }
+
+
+// Estructura Condicional Anidada
+if($a > $b) {
+    echo "El valor de a es mayor que el valor de b";
+} else if($a < $b) {
+    echo "El valor de a es menor que el valor de b";
+} else {
+    echo "El valor de a es igual que el valor de b";
+}
