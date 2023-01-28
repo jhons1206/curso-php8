ad40bbc5fa633590e3c6da5b3159a8b12cf24a70
jhons1206 <webdeveloper84@gmail.com>
2023-01-28 09:02:08 -0500

    Sección 16: Arrays unidimensionales - COMPLETADO

---
+++ 16-Arrays-unidimensionales/index.php
@@ -0,0 +1,24 @@
+<?php
+// Arrays Unidimensionales
+
+// $array = [10, 13, 15, 20];
+
+// $array[] = 10;
+// $array[] = 13;
+// $array[] = 15;
+// $array[] = 20;
+// $array[] = 17;
+
+// echo $array[4];
+
+
+
+// $array = [10, "Jhon", 15, 20, false];
+
+$datos = [
+    'nombre' => 'Jhon Sandoval',
+    'email' => 'webdeveloper84@gmail.com',
+    'celular' => '964174598'
+];
+
+echo $datos['celular'];
